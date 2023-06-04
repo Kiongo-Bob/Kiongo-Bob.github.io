24baebefc7c19f3a7de3588f226b2c31a40ff296
Kiongo-Bob <njoguub@gmail.com>
2023-06-04 20:36:50 +0300

    Projects

--- imgs/rsize.py
+++ imgs/rsize.py
@@ -1,17 +1,17 @@
 import cv2
 
 # Read the image
-image = cv2.imread('yt/imgs/work3.jpg')
+image = cv2.imread('Kiongo-Bob.github.io/imgs/Me.jpg')
 
 # Define the new dimensions
-new_width = 500
-new_height = 500
+new_width = 585
+new_height = 796
 
 # Resize the image
 resized_image = cv2.resize(image, (new_width, new_height))
 
 # Save the resized image
-cv2.imwrite('./work3.jpg', resized_image)
+cv2.imwrite('me.jpg', resized_image)
 
 # Display the original and resized images
 cv2.imshow('Resized Image', resized_image)
