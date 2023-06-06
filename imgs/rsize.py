082585286fa1fdff4fe3cb9161c321e99552c216
Kiongo-Bob <njoguub@gmail.com>
2023-06-06 13:20:53 +0300

    Update gitignore

--- imgs/rsize.py
+++ imgs/rsize.py
@@ -1,17 +1,17 @@
 import cv2
 
 # Read the image
-image = cv2.imread('Kiongo-Bob.github.io/imgs/Me.jpg')
+image = cv2.imread('/home/kiongobob/Project Based Learning/Kiongo-Bob.github.io/imgs/Me1.jpg')
 
 # Define the new dimensions
-new_width = 585
-new_height = 796
+new_width = 600
+new_height = 800
 
 # Resize the image
 resized_image = cv2.resize(image, (new_width, new_height))
 
 # Save the resized image
-cv2.imwrite('me.jpg', resized_image)
+cv2.imwrite('/home/kiongobob/Project Based Learning/Kiongo-Bob.github.io/imgs/Me2.jpg', resized_image)
 
 # Display the original and resized images
 cv2.imshow('Resized Image', resized_image)
